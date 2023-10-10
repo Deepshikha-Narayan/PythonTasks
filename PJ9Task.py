e2e77240f110eede855adaff198380c54c739079
Deepshikha-Narayan <147501045+Deepshikha-Narayan@users.noreply.github.com>
2023-10-10 17:39:38 +0530

    Add files via upload

---
+++ PJ9Task.py
@@ -0,0 +1,10 @@
+#Always use int when want to print integer value
+
+a=int(input("Enter 1st number"))
+b=int(input("Enter 2nd number"))
+c=int(input("Enter 3rd number"))
+d=int(input("Enter 4th number"))
+e=int(input("Enter 5th number"))
+list={a,b,c,d,e}
+print(list)
+
