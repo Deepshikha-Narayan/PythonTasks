9e7a8ea59f9afd1c8d9ab404b5a1c1d655d080b6
Deepshikha-Narayan <147501045+Deepshikha-Narayan@users.noreply.github.com>
2023-10-16 14:36:26 +0530

    PJ19AO.py

--- PJ19AO.py
+++ PJ19AO.py
@@ -1,5 +1,8 @@
 a=int(input("Enter 1st number"))
 b=int(input("Enter 2nd number"))
-print(a==b)
-print(a>=b)
-print(a<=b)
+if a > b:
+    print("A is greater than B")
+elif a < b:
+    print("B is greater than A")
+else:
+    print("A is equal to B")
